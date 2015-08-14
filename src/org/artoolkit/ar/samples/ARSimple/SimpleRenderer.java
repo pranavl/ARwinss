45101652af9e1f39c743e2ef26cabd041b2707b8
Pranav Lakshminarayanan <pranz.laks@gmail.com>
2015-08-14 16:29:23 -0400

    Style formatting

--- src/org/artoolkit/ar/samples/ARSimple/SimpleRenderer.java
+++ src/org/artoolkit/ar/samples/ARSimple/SimpleRenderer.java
@@ -1,50 +1,6 @@
 /*
  *  SimpleRenderer.java
  *  ARToolKit5
- *
- *  Disclaimer: IMPORTANT:  This Daqri software is supplied to you by Daqri
- *  LLC ("Daqri") in consideration of your agreement to the following
- *  terms, and your use, installation, modification or redistribution of
- *  this Daqri software constitutes acceptance of these terms.  If you do
- *  not agree with these terms, please do not use, install, modify or
- *  redistribute this Daqri software.
- *
- *  In consideration of your agreement to abide by the following terms, and
- *  subject to these terms, Daqri grants you a personal, non-exclusive
- *  license, under Daqri's copyrights in this original Daqri software (the
- *  "Daqri Software"), to use, reproduce, modify and redistribute the Daqri
- *  Software, with or without modifications, in source and/or binary forms;
- *  provided that if you redistribute the Daqri Software in its entirety and
- *  without modifications, you must retain this notice and the following
- *  text and disclaimers in all such redistributions of the Daqri Software.
- *  Neither the name, trademarks, service marks or logos of Daqri LLC may
- *  be used to endorse or promote products derived from the Daqri Software
- *  without specific prior written permission from Daqri.  Except as
- *  expressly stated in this notice, no other rights or licenses, express or
- *  implied, are granted by Daqri herein, including but not limited to any
- *  patent rights that may be infringed by your derivative works or by other
- *  works in which the Daqri Software may be incorporated.
- *
- *  The Daqri Software is provided by Daqri on an "AS IS" basis.  DAQRI
- *  MAKES NO WARRANTIES, EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION
- *  THE IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS
- *  FOR A PARTICULAR PURPOSE, REGARDING THE DAQRI SOFTWARE OR ITS USE AND
- *  OPERATION ALONE OR IN COMBINATION WITH YOUR PRODUCTS.
- *
- *  IN NO EVENT SHALL DAQRI BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL
- *  OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- *  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- *  INTERRUPTION) ARISING IN ANY WAY OUT OF THE USE, REPRODUCTION,
- *  MODIFICATION AND/OR DISTRIBUTION OF THE DAQRI SOFTWARE, HOWEVER CAUSED
- *  AND WHETHER UNDER THEORY OF CONTRACT, TORT (INCLUDING NEGLIGENCE),
- *  STRICT LIABILITY OR OTHERWISE, EVEN IF DAQRI HAS BEEN ADVISED OF THE
- *  POSSIBILITY OF SUCH DAMAGE.
- *
- *  Copyright 2015 Daqri, LLC.
- *  Copyright 2011-2015 ARToolworks, Inc.
- *
- *  Author(s): Julian Looser, Philip Lamb
- *
  */
 package org.artoolkit.ar.samples.ARSimple;
 
@@ -54,10 +10,11 @@ import java.util.logging.Logger;
 import javax.microedition.khronos.opengles.GL10;
 
 import org.artoolkit.ar.base.ARToolKit;
+import org.artoolkit.ar.base.readers.STLReader;
 import org.artoolkit.ar.base.rendering.ARRenderer;
 import org.artoolkit.ar.base.rendering.Cube;
 import org.artoolkit.ar.base.rendering.Pyramid;
-import org.artoolkit.ar.base.rendering.Surface;
+import org.artoolkit.ar.base.rendering.STLSurface;
 
 /**
  * A renderer that adds a marker and draws an object on it.
@@ -65,22 +22,24 @@ import org.artoolkit.ar.base.rendering.Surface;
 public class SimpleRenderer extends ARRenderer {
 
     /**
-     * Marker ID.
+     * Markers.
      */
-    private int markerID = -1;
-    private int markerID2 = -1;
-
+    private int markerHiro = -1;
+    private int markerKanji = -1;
+    private int markerA = -1;
+    
     /**
      * Cube visualization.
      */
-    private Cube cube = new Cube(40.0f, 0.0f, 0.0f, 20.0f);
+    private Cube cube = new Cube(60.0f, 80.0f, 0.0f, 30.0f);
 
     /**
      * Pyramid visualization.
      */
-    private Pyramid pyr = new Pyramid(40.0f);
+    private Pyramid pyr = new Pyramid(40.0f, 0.0f, 0.0f, 0.0f, 10.0f, 10.0f);
 
-    //private Surface sur;
+    private STLSurface sur;
+    
     /**
      * Markers can be configured here.
      *
@@ -89,11 +48,17 @@ public class SimpleRenderer extends ARRenderer {
     @Override
     public boolean configureARScene() {
 
-        markerID = ARToolKit.getInstance().addMarker(
+        markerHiro = ARToolKit.getInstance().addMarker(
                 "single;Data/patt.hiro;80");
-        markerID2 = ARToolKit.getInstance().addMarker(
+        markerKanji = ARToolKit.getInstance().addMarker(
                 "single;Data/patt.kanji;80");
-        if (markerID < 0 || markerID2 < 0) {
+        markerA = ARToolKit.getInstance().addMarker(
+                "single;Data/multi/patt.a;80");
+        
+        pyr.rotateX((float) (Math.PI / 2));
+        pyr.translate(0.0f, -155.0f, 0.0f);
+        
+        if (markerHiro < 0 || markerKanji < 0 || markerA < 0) {
             return false;
         }
         return true;
@@ -102,7 +67,7 @@ public class SimpleRenderer extends ARRenderer {
     /**
      * Override the draw function from ARRenderer.
      *
-     * @param gl
+     * @param gl .
      */
     @Override
     public void draw(GL10 gl) {
@@ -118,17 +83,24 @@ public class SimpleRenderer extends ARRenderer {
         gl.glEnable(GL10.GL_DEPTH_TEST);
         gl.glFrontFace(GL10.GL_CW);
 
-        // If the marker is visible, apply its transformation, and draw a shape
-        if (ARToolKit.getInstance().queryMarkerVisible(markerID2)) {
+        //If the marker is visible, draw a shape
+        if (ARToolKit.getInstance().queryMarkerVisible(markerA)) {
             gl.glMatrixMode(GL10.GL_MODELVIEW);
             gl.glLoadMatrixf(ARToolKit.getInstance().
-                    queryMarkerTransformation(markerID2), 0);
+                    queryMarkerTransformation(markerA), 0);
             cube.draw(gl);
         }
-        if (ARToolKit.getInstance().queryMarkerVisible(markerID)) {
+//        if (ARToolKit.getInstance().queryMarkerVisible(markerHiro)) {
+//            gl.glMatrixMode(GL10.GL_MODELVIEW);
+//            gl.glLoadMatrixf(ARToolKit.getInstance().
+//                    queryMarkerTransformation(markerHiro), 0);
+//            sur.draw(gl);
+//        }
+        
+        if (ARToolKit.getInstance().queryMarkerVisible(markerKanji)) {
             gl.glMatrixMode(GL10.GL_MODELVIEW);
             gl.glLoadMatrixf(ARToolKit.getInstance().
-                    queryMarkerTransformation(markerID), 0);
+                    queryMarkerTransformation(markerKanji), 0);
             pyr.draw(gl);
         }
 
